22c20755a97df05305ac478243e2f6f90658cd8c
Tahamidul Haque <tahmidulanon@gmail.com>
2022-08-10 23:46:24 +0600

    Add files via upload

---
+++ FT_TASK_2/js/myscripts.js
@@ -0,0 +1,6 @@
+function afterclicked(){
+var a=document.getElementById("myprint"); //ei line project e usable
+a.innerHTML="Not hellow world"; //ei line project e usable
+var b=document.getElementById("mybad");
+b.innerHTML="The "+a.innerHTML+" is good";
+}
